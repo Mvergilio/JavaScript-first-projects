d6931e3678c0662ac8b0051f2e00d68c78a327fc
Mvergilio <mvergiliodesouza@gmail.com>
2021-10-31 11:15:48 -0300

    color-flipper is finished

--- color-Flipper/hex.js
+++ color-Flipper/hex.js
@@ -1,1 +1,20 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const body = document.querySelector('body');
+const span = document.querySelector('span');
+const btn = document.getElementById('btn');
+console.log(body, span, btn);
+btn.addEventListener('click', function() {
+    let i = 0,
+        randomIndex = 0,
+        temporaryNum = "",
+        newColor = '#';
+    while (i < 6) {
+        randomIndex = Math.floor(Math.random() * hex.length);
+        temporaryNum = hex[randomIndex].toString();
+        newColor += temporaryNum;
+        i++;
+    }
+    body.style.backgroundColor = newColor;
+    span.innerText = newColor;
+    console.log(newColor);
+})
