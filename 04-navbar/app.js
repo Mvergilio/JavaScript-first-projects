c4aa1a03ceb8cb104a405a7c95b9f88302954c55
Mvergilio <mvergiliodesouza@gmail.com>
2021-11-01 12:04:49 -0300

    toggle menu is done

--- 04-navbar/app.js
+++ 04-navbar/app.js
@@ -3,3 +3,22 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+const toggleBtn = document.querySelector('.nav-toggle');
+const menuList = document.querySelector('#btn');
+
+//toggle (show-links) class
+toggleBtn.addEventListener('click', function() {
+    let myMenu = menuList.classList;
+
+    console.log(myMenu.contains('show-links'))
+
+
+    console.log(myMenu);
+
+
+    if (myMenu.contains('show-links') === false) {
+        myMenu.add('show-links');
+    } else {
+        myMenu.remove('show-links');
+    }
+})
