53aee702fb40fb304057f1c8b2cdf7ff2bb6c316
Mvergilio <mvergiliodesouza@gmail.com>
2021-11-02 13:04:27 -0300

    First toggle button is working

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -1,2 +1,21 @@
 //using selectors inside the element
 // traversing the dom
+const questionSection = document.querySelector('.question');
+const listSection = questionSection.children;
+
+// select the button
+const minusPlusBtn = document.querySelector('.question-btn');
+const btnSpan = minusPlusBtn.children;
+minusPlusBtn.addEventListener("click", function() {
+    if (listSection[1].classList.contains('show-text')) {
+        listSection[1].classList.remove('show-text');
+    } else {
+        listSection[1].classList.add('show-text');
+    }
+    if (listSection[1].classList.contains('question-text')) {
+        listSection[1].classList.remove('question-text');
+
+    } else {
+        listSection[1].classList.add('question-text');
+    }
+})
