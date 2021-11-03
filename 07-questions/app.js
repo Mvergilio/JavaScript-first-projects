de400828907db03c1e581cbc590865b06c7ae976
Mvergilio <mvergiliodesouza@gmail.com>
2021-11-03 12:32:03 -0300

    question porject is ready

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -1,19 +1,58 @@
-const btns = document.querySelectorAll('.question-btn');
-
-btns.forEach(function(btn) {
-    btn.addEventListener('click', function(e) {
-        const question = e.currentTarget.parentElement.parentElement;
-        question.classList.toggle('show-text')
-        console.log(question);
+const questions = document.querySelectorAll('.question');
+// console.log(questions);
+questions.forEach(function(question) {
+    const btn = question.querySelector('.question-btn');
+    // console.log(btn);
+    btn.addEventListener('click', function() {
+        questions.forEach(function(item) {
+            if (item !== question) {
+                item.classList.remove('show-text')
+            }
+            question.classList.toggle('show-text');
+        })
     })
 })
 
 
-// const questions = document.querySelectorAll('.question');
-// questions.forEach(function(elem) {
-//     const btns = elem.querySelector('.question-btn')
-//     btns.addEventListener('click', function(e) {
-//         console.log(e);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const questions = document.querySelectorAll('.question');
+// console.log(questions)
+
+// questions.forEach(function(question) {
+
+//     const btn = question.querySelector('.question-btn');
+
+//     btn.addEventListener('click', function() {
+//         questions.forEach(function(item) {
+
+//             if (item !== question) {
+//                 item.classList.remove('show-text')
+//             }
+//         })
+//         question.classList.toggle('show-text');
+
+//     })
+// })
+
+
+// btns.forEach(function(btn) {
+//     btn.addEventListener('click', function(e) {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text')
+//         console.log(question);
 //     })
 // })
