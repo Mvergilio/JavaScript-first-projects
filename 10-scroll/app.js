4d7a6125a958750ae62ff7ea7e0c1107401a1217
Mvergilio <mvergiliodesouza@gmail.com>
2021-11-09 12:56:17 -0300

    Trying to create an smooth scrolling but not succefull

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -4,10 +4,42 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const date = document.querySelector('.date');
+
+// get the  upated date
+const currentDate = new Date().getFullYear();
+date.innerHTML = currentDate;
 
 // ********** close links ************
+const sectionLocation = document.querySelectorAll('.section');
+const sectionToLinks = document.querySelectorAll('.scroll-link');
+//console.log(sectionLocation, sectionToLinks);
 
-// ********** fixed navbar ************
 
+sectionToLinks.forEach((item) => {
+        item.addEventListener('click', (e) => {
+            let currentLink = e.currentTarget.innerHTML;
+
+            //console.log(currentLink);
+            sectionLocation.forEach((item) => {
+                let position = item.getBoundingClientRect().y.toFixed();
+                let identification = item.id;
+                if (currentLink === identification) {
+                    e.currentTarget.style.transition = '5s ease-out';
+
+                }
+            })
+        })
+    })
+    // ********** fixed navbar ************
+function fixedNav() {
+    const navbar = document.querySelector('.links-container');
+    if (window.pageYOffset >= 340) {
+        return navbar.classList.add('fixed-nav');
+    } else {
+        return navbar.classList.remove('fixed-nav');
+    }
+}
+fixedNav();
 // ********** smooth scroll ************
 // select links
