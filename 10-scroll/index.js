48b0497e504285a5b67ffb558493f467b80caef7
Mvergilio <mvergiliodesouza@gmail.com>
2021-11-17 20:25:29 -0300

    fineshed the toggle menu/scrollToTop button/fixed nav Bar

--- 10-scroll/index.js
+++ 10-scroll/index.js
@@ -1,17 +1,40 @@
+// global variables
+
 const navContainer = document.querySelector("#nav");
 const linksContainer = document.querySelector('.links-container');
-
+const links = document.querySelector('.links');
 //getting the button
+
 const menuToggle = document.querySelector('.nav-toggle');
+
 //menu toggle event
 
 menuToggle.addEventListener('click', () => {
-    const linksHeight = linksContainer.getBoundingClientRect().height;
-    const navHeight = navContainer.getBoundingClientRect().height;
-    if (navHeight === 0) {
-        console.log(linksContainer)
-        navContainer.style.height = `${linksHeight}px`;
+    const linksHeight = links.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    if (containerHeight === 0) {
+
+        linksContainer.style.height = `${linksHeight}px`;
     } else {
-        navContainer.style.height = `0px`;
+        linksContainer.style.height = 0;
+    }
+})
+
+// fixed navBar
+const toTopButton = document.querySelector('.top-link ')
+const navBarHeight = navContainer.getBoundingClientRect().height;
+window.addEventListener('scroll', () => {
+    let windowScroll = window.scrollY;
+    if (windowScroll > navBarHeight) {
+        navContainer.classList.add('fixed-nav');
+    } else {
+        navContainer.classList.remove('fixed-nav');
+    }
+
+    // button to top of the page
+    if (windowScroll > 500) {
+        toTopButton.classList.add('show-link')
+    } else {
+        toTopButton.classList.remove('show-link');
     }
 })
