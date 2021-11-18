1e2bf40691335b85a3dba34705ba798aaba81b02
Mvergilio <mvergiliodesouza@gmail.com>
2021-11-17 22:14:09 -0300

    Started the smooth scroll efect

--- 10-scroll/index.js
+++ 10-scroll/index.js
@@ -3,6 +3,8 @@
 const navContainer = document.querySelector("#nav");
 const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links');
+const footerDateSpan = document.querySelector('.date');
+const linksToSection = document.querySelectorAll('.scroll-link');
 //getting the button
 
 const menuToggle = document.querySelector('.nav-toggle');
@@ -24,17 +26,42 @@ menuToggle.addEventListener('click', () => {
 const toTopButton = document.querySelector('.top-link ')
 const navBarHeight = navContainer.getBoundingClientRect().height;
 window.addEventListener('scroll', () => {
-    let windowScroll = window.scrollY;
-    if (windowScroll > navBarHeight) {
-        navContainer.classList.add('fixed-nav');
-    } else {
-        navContainer.classList.remove('fixed-nav');
-    }
+        let windowScroll = window.scrollY;
+        if (windowScroll > navBarHeight) {
+            navContainer.classList.add('fixed-nav');
+        } else {
+            navContainer.classList.remove('fixed-nav');
+        }
 
-    // button to top of the page
-    if (windowScroll > 500) {
-        toTopButton.classList.add('show-link')
-    } else {
-        toTopButton.classList.remove('show-link');
-    }
+        // button to top of the page
+        if (windowScroll > 500) {
+            toTopButton.classList.add('show-link')
+        } else {
+            toTopButton.classList.remove('show-link');
+        }
+    })
+    // setting the footer current date
+function footerDate() {
+    let currentDate = new Date().getFullYear();
+    return footerDateSpan.innerHTML = currentDate;
+
+}
+footerDate();
+
+// smooth scrolling settings
+linksToSection.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        //prevent defaut setting
+        e.preventDefault();
+        const sectionId = e.currentTarget.getAttribute('href').slice(1);
+        const eachSectionToGo = document.getElementById(sectionId);
+        const sectionPosition = eachSectionToGo.offsetTop;
+        window.scrollTo({
+            left: 0,
+            behavior: "smooth",
+            top: sectionPosition
+        })
+
+
+    })
 })
