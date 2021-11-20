7ea7d9cdefe311ac52e6c25cb9ac35542063e35b
Mvergilio <mvergiliodesouza@gmail.com>
2021-11-20 12:30:44 -0300

    Started the logic that evolves around the 12-countDown

--- 12-countdown/app.js
+++ 12-countdown/app.js
@@ -1,23 +1,40 @@
 const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
 ];
 const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
 ];
+
+// get the days/hours/minutes/seconds tags
+const days = document.querySelector('.days');
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.mins');
+const seconds = document.querySelector('.secs');
+
+// setting the current Date and the noted Date
+const currentDate = new Date().getTime();
+const notedDate = new Date('December 25,  2021 8:00:00 GMT+00:00').getTime();
+const dayElapse = notedDate - currentDate;
+// days
+const acurateDay = Math.ceil(dayElapse / (1000 * 3600 * 24));
+console.log(acurateDay);
+days.innerHTML = acurateDay;
+// hours
+const acurateHour = Math.ceil(dayElapse / (1000 * 3600));
