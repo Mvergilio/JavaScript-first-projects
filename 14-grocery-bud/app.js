ec98bc514a89ef45aad4f448d7cf3ca0c161ad61
Mvergilio <mvergiliodesouza@gmail.com>
2021-11-28 20:28:17 -0300

    started the logic around the 1-grocery bud

--- 14-grocery-bud/app.js
+++ 14-grocery-bud/app.js
@@ -1,5 +1,10 @@
 // ****** SELECT ITEMS **********
-
+const ALERT = document.querySelector('.alert');
+const INPUT_TEXT = document.getElementById('grocery');
+const SUBMIT_BTN = document.querySelector('.submit-btn');
+const TEXT_TITLE = document.querySelector('.title');
+const BUTTON_CONTAINER = document.querySelector('.btn-container');
+const CLEAR_BTN = document.querySelector('.clear-btn');
 // edit option
 
 // ****** EVENT LISTENERS **********
