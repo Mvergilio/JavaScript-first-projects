63b0c99c4335e6ef307c535368f805f09b8cc19d
Mvergilio <mvergiliodesouza@gmail.com>
2021-12-14 12:23:50 -0300

    the edit feature is finished

--- 14-grocery-bud/app.js
+++ 14-grocery-bud/app.js
@@ -13,44 +13,75 @@ let editId = "";
 let editElement;
 
 // ****** EVENT LISTENERS **********
-FORM.addEventListener('submit', addItem);
-
-// ****** LOCAL STORAGE **********
+FORM.addEventListener("submit", addItem);
 
 // ****** FUNCTIONS **********
 // submit item
 function addItem(e) {
-    e.preventDefault();
-    let inputValue = INPUT_TEXT.value;
-    let article = document.createElement('article');
-    article.classList.add('grocery-item');
-    let place_holder = `<p class="title">${inputValue}</p>
+	e.preventDefault();
+	let id = new Date().getTime();
+	let inputValue = INPUT_TEXT.value;
+	let article = document.createElement("article");
+	article.classList.add("grocery-item");
+	article.setAttribute("data-id", id);
+	let place_holder = `<p class="title">${inputValue}</p>
     <div class="btn-container">
         <button class="edit-btn">
           <i class="fas fa-edit"></i>
         </button>
         <button class="delete-btn">
-          <i class="fas fa-trash"></i>
+        <i class="fas fa-trash"></i>
         </button>
-    </div>`;
-    article.innerHTML = place_holder;
-    if (inputValue && !editFlag) {
-        LIST_CONTAINER.appendChild(article);
-        CONTAINER.classList.add('show-container');
-        showAlert('item added to the list', 'success')
-    } else if (!inputValue && !editFlag) {
-        showAlert('please enter value', 'danger')
-    }
-    // SETTING THE EDDIT FUNCTIONALITY
+        </div>`;
+	article.innerHTML = place_holder;
+
+	if (inputValue && !editFlag) {
+		LIST_CONTAINER.appendChild(article);
+		CONTAINER.classList.add("show-container");
+		showAlert("item added to the list", "success");
+	} else if (!inputValue && !editFlag) {
+		showAlert("please enter value", "danger");
+	} else if (!inputValue && editFlag) {
+		showAlert("please enter value", "danger");
+	} else {
+		editElement.textContent = INPUT_TEXT.value;
+		setBackToDefault();
+	}
+	// setting the edit button
+	const EDIT_BTN = document.querySelectorAll(".edit-btn");
+	const DELETE_BTN = document.querySelector(".delete-btn");
+	EDIT_BTN.forEach((element) => {
+		element.addEventListener("click", editItem);
+	});
+}
+// alert menssage
+function showAlert(text, type) {
+	ALERT.textContent = text;
+	ALERT.classList.add(`alert-${type}`);
+	setTimeout(() => {
+		ALERT.textContent = "";
+		ALERT.classList.remove(`alert-${type}`);
+	}, 1000);
 }
 
-function showAlert(text, type) {
-    ALERT.textContent = text;
-    ALERT.classList.add(`alert-${type}`);
-    setTimeout(() => {
-        ALERT.textContent = '';
-        ALERT.classList.remove(`alert-${type}`);
-    }, 1000);
+// edit Functionality
+function editItem(e) {
+	editId = e.currentTarget.parentElement.parentElement.dataset.id;
+	editElement =
+		e.currentTarget.parentElement.parentElement.querySelector(".title");
+	let value = editElement.textContent;
+	INPUT_TEXT.value = value;
+	editFlag = true;
+	SUBMIT_BTN.textContent = "edit";
 }
+
+function setBackToDefault() {
+	SUBMIT_BTN.textContent = "submit";
+	editFlag = false;
+	editId = "";
+	editElement = null;
+}
+
+// ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
