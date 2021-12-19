212a8e701b6790a39fb175c2c51a89480fc94b15
Mvergilio <mvergiliodesouza@gmail.com>
2021-12-19 07:30:58 -0300

    finished the 15 projects

--- 15-slide/app.js
+++ 15-slide/app.js
@@ -1,21 +1,26 @@
-const SLIDES = document.querySelectorAll('.slide');
-const PREV_BTN = document.querySelector('.prevBtn');
-const NEXT_BTN = document.querySelector('.nextBtn');
+const SLIDES = document.querySelectorAll(".slide");
+const PREV_BTN = document.querySelector(".prevBtn");
+const NEXT_BTN = document.querySelector(".nextBtn");
 SLIDES.forEach((slide, index) => {
-    slide.style.left = `${index * 100}`
-})
+	slide.style.left = `${index * 100}%`;
+});
 let counter = 0;
-NEXT_BTN.addEventListener('click', () => {
-    counter++;
-    showingSlide();
-})
-PREV_BTN.addEventListener('click', () => {
-    counter--;
-    showingSlide();
-})
+NEXT_BTN.addEventListener("click", () => {
+	counter++;
+	showingSlide();
+});
+PREV_BTN.addEventListener("click", () => {
+	counter--;
+	showingSlide();
+});
 
 function showingSlide() {
-    SLIDES.forEach((slide) => {
-        slide.style.transform = `traslateX(-${counter * 100}%)`
-    })
+	if (counter === SLIDES.length) {
+		counter = 0;
+	} else if (counter < 0) {
+		counter = SLIDES.length - 1;
+	}
+	SLIDES.forEach((slide) => {
+		slide.style.transform = `translateX(-${counter * 100}%)`;
+	});
 }
